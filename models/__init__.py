c166d63bbf15a576aba4ad16f5c306c46bf1f601
Ldoun <ldoun8260@gmail.com>
2023-12-14 15:37:28 +0900

    feat: arguments

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
 from models.vae import VAE
 
 def args_for_model(parser, model):
-    parser.add_argument('--something', type=str, default="?", help="?")
+    parser.add_argument('--act', type=str, default="ELU", help="activate function in VAE")
