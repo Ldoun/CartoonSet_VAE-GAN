c968b84b921194730428f44f0170a02544c6f273
Ldoun <ldoun8260@gmail.com>
2023-12-14 16:36:16 +0900

    fix: image preprocessing

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 import torch
 from torch.utils.data import  Dataset
-from torchvision.transforms.functional import to_tensor
+import torchvision.transforms as transform
 
 import os
 from PIL import Image
@@ -9,10 +9,15 @@ from glob import glob
 class DataSet(Dataset):
     def __init__(self, base_path, label=None):
         self.files = glob(os.path.join(base_path, '*', '*.png'))
+        self.t = transform.Compose([
+            transform.ToTensor(),
+            transform.CenterCrop([412, 412]),
+            transform.Resize([128, 128])
+        ])
 
     def __len__(self):
         return len(self.files)
     
     def __getitem__(self, index):
-        image = to_tensor(Image.open(self.files[index]))
+        image = self.t(Image.open(self.files[index]).convert('RGB'))
         return image
