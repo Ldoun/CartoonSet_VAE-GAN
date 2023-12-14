ecc526c1a59941cc25f788b261fe5deea49fcc96
Ldoun <ldoun8260@gmail.com>
2023-12-14 20:45:42 +0900

    feat: train gan

---
+++ main_gan.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import logging
+from functools import partial
+from sklearn.model_selection import train_test_split
+
+import torch
+from torch import optim, nn
+from torch.utils.data import DataLoader, Subset
+
+from models import Discriminator, Generator
+from data import DataSet
+from trainer_gan import Trainer
+from config import get_args
+from lr_scheduler import get_sch
+from utils import seed_everything, handle_unhandled_exception, save_to_json
+
+if __name__ == "__main__":
+    args = get_args()
+    seed_everything(args.seed) #fix seed
+    device = torch.device('cuda:0') #use cuda:0
+
+    result_path = os.path.join(args.result_path, args.model +'_'+str(len(os.listdir(args.result_path))))
+    os.makedirs(result_path)
+
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
+    logger = logging.getLogger()
+    logger.addHandler(logging.FileHandler(os.path.join(result_path, 'log.log')))    
+    logger.info(args)
+    save_to_json(vars(args), os.path.join(result_path, 'config.json'))
+    sys.excepthook = partial(handle_unhandled_exception,logger=logger)
+    
+    fold=0
+    dataset = DataSet(base_path=args.path)
+    train_index, valid_index = train_test_split(range(len(dataset)), test_size=0.2)
+
+    logger.info(f'start training of {fold+1}-fold')
+
+    train_dataset = Subset(dataset, train_index)
+    valid_dataset = Subset(dataset, valid_index)
+
+    G_model = Generator(args.act).to(device)
+    D_model = Discriminator(args.act).to(device)
+    
+    loss_fn = torch.nn.BCELoss()
+
+    optimizer_G = torch.optim.Adam(G_model.parameters(), lr=args.lr)
+    optimizer_D = torch.optim.Adam(D_model.parameters(), lr=args.lr)
+
+    train_loader = DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, #pin_memory=True
+    )
+    valid_loader = DataLoader(
+        valid_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers, #pin_memory=True
+    )
+    
+    trainer = Trainer(
+        train_loader, valid_loader, [G_model, D_model], loss_fn, [optimizer_G, optimizer_D], device, args.patience, args.epochs, result_path, logger, len(train_dataset), len(valid_dataset))
+    trainer.train() #start training
---
+++ trainer_gan.py
@@ -0,0 +1,101 @@
+import os
+import sys
+import torch
+import numpy as np
+from tqdm import tqdm
+
+class Trainer():
+    def __init__(self, train_loader, valid_loader, model, loss_fn, optimizer, device, patience, epochs, result_path, fold_logger, len_train, len_valid):
+        self.train_loader = train_loader
+        self.valid_loader = valid_loader
+        self.generator, self.discriminator = model
+        self.loss_fn = loss_fn
+        self.optimizer_G, self.optimizer_D = optimizer
+        self.device = device
+        self.patience = patience
+        self.epochs = epochs
+        self.logger = fold_logger
+        self.best_model_path = os.path.join(result_path, 'best_model.pt')
+        self.len_train = len_train
+        self.len_valid = len_valid
+    
+    def train(self):
+        best = np.inf
+        for epoch in range(1,self.epochs+1):
+            loss_G_train, loss_D_train = self.train_step()
+            loss_G_val, loss_D_val = self.valid_step()
+
+            self.logger.info(f'Epoch {str(epoch).zfill(5)}: GT_loss:{loss_G_train:.3f} DT_loss:{loss_D_train:.3f} GV_loss:{loss_G_val:.3f} DV_loss:{loss_D_val:.3f}')
+
+            if loss_G_val < best:
+                best = loss_G_val
+                torch.save(self.generator.state_dict(), self.best_model_path)
+                bad_counter = 0
+
+            else:
+                bad_counter += 1
+
+            if bad_counter == self.patience:
+                break
+
+    def train_step(self):
+        self.generator.train()
+        self.discriminator.train()
+
+        total_G_loss, total_D_loss = 0, 0
+        for batch in tqdm(self.train_loader, file=sys.stdout): #tqdm output will not be written to logger file(will only written to stdout)
+            batch = batch.to(self.device)
+            valid = torch.ones((batch.shape[0], 1), requires_grad=False, dtype=torch.float, device=self.device)
+            fake = torch.zeros((batch.shape[0], 1), requires_grad=False, dtype=torch.float, device=self.device)
+
+            self.optimizer_G.zero_grad()
+            z = torch.randn((batch.shape[0], 512), device=self.device)
+            gen_imgs = self.generator(z)
+            g_loss = self.loss_fn(self.discriminator(gen_imgs), valid)
+            g_loss.backward()
+            self.optimizer_G.step()
+
+            self.optimizer_D.zero_grad()
+            real_loss = self.loss_fn(self.discriminator(batch), valid)
+            fake_loss = self.loss_fn(self.discriminator(gen_imgs.detach()), fake)
+            d_loss = (real_loss + fake_loss) / 2
+            d_loss.backward()
+            self.optimizer_D.step()
+
+            total_G_loss += g_loss.item() * batch.shape[0]
+            total_D_loss += d_loss.item() * batch.shape[0]
+        
+        return total_G_loss/self.len_train, total_D_loss/self.len_train
+    
+    def valid_step(self):
+        self.model.eval()
+        with torch.no_grad():
+            total_G_loss, total_D_loss = 0, 0
+            for batch in self.valid_loader:
+                batch = batch.to(self.device)
+                valid = torch.ones((batch.shape[0], 1), requires_grad=False, dtype=torch.float, device=self.device)
+                fake = torch.zeros((batch.shape[0], 1), requires_grad=False, dtype=torch.float, device=self.device)
+                z = torch.randn((batch.shape[0], 512), device=self.device)
+                gen_imgs = self.generator(z)
+                g_loss = self.loss_fn(self.discriminator(gen_imgs), valid)
+
+                real_loss = self.loss_fn(self.discriminator(batch), valid)
+                fake_loss = self.loss_fn(self.discriminator(gen_imgs.detach()), fake)
+                d_loss = (real_loss + fake_loss) / 2
+
+                total_G_loss += g_loss.item() * batch.shape[0]
+                total_D_loss += d_loss.item() * batch.shape[0]
+                
+        return total_G_loss/self.len_valid, total_D_loss/self.len_valid
+
+    # def test(self, test_loader):
+    #     self.model.load_state_dict(torch.load(self.best_model_path))
+    #     self.model.eval()
+    #     with torch.no_grad():
+    #         result = []
+    #         for batch in test_loader:
+    #             x = batch['data'].to(self.device)
+    #             output, _, _ = self.model(x).detach().cpu().numpy()
+    #             result.append(output)
+
+    #     return np.concatenate(result,axis=0)
