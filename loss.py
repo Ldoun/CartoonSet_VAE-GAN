eae0f981506fb80589d8704efd896f70fe065192
Ldoun <ldoun8260@gmail.com>
2023-12-14 15:33:57 +0900

    feat: vae loss

---
+++ loss.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn.functional as F
+
+def vae_loss(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy_with_logits(recon_x, x, size_average=False)
+    KLD_element = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
+    KLD = torch.sum(KLD_element).mul_(-0.5)
+    return BCE + KLD
