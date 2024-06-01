241cdb5832167059a75d2f4ac75281059561e47c
Ldoun <ldoun8260@gmail.com>
2024-06-01 15:32:20 +0900

    fix: dataset

--- data.py
+++ data.py
@@ -7,8 +7,8 @@ from PIL import Image
 from glob import glob
 
 class DataSet(Dataset):
-    def __init__(self, base_path, is_for_gan=False):
-        self.files = glob(os.path.join(base_path, '*', '*.png'))
+    def __init__(self, base_path, is_for_gan=False, is_for_anomaly=False):
+        self.files = glob(os.path.join(base_path, '*.png')) if is_for_anomaly else glob(os.path.join(base_path, '*', '*.png'))
         
         if is_for_gan:
             self.t = transform.Compose([
@@ -17,12 +17,17 @@ class DataSet(Dataset):
                 transform.Resize([128, 128], antialias=True),
                 transform.Normalize([0.5], [0.5])
             ])
+        elif is_for_anomaly:
+            self.t = transform.Compose([
+                transform.ToTensor(),
+            ])
         else:
             self.t = transform.Compose([
                 transform.ToTensor(),
                 transform.CenterCrop([412, 412]),
                 transform.Resize([128, 128], antialias=True)
             ])
+        
 
     def __len__(self):
         return len(self.files)
